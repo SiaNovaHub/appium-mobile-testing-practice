c9898fd3285893412e119bee7b7ccae46e744586
SiaNovaHub <nast.zelenova2018@yandex.ru>
2026-04-12 20:23:25 +0200

    Updated SetWifiSettings test and added LongPressMenuValidation, added longPressAction util

--- src/main/java/sianova/AppiumBase.java
+++ src/main/java/sianova/AppiumBase.java
@@ -1,22 +1,50 @@
 package sianova;
 import io.appium.java_client.AppiumBy;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.options.UiAutomator2Options;
-import io.appium.java_client.service.local.AppiumDriverLocalService;
-import io.appium.java_client.service.local.AppiumServiceBuilder;
-import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 
 public class AppiumBase extends BaseTest {
     @Test
-    public void SetWifiSettings() throws URISyntaxException, MalformedURLException {
+    public void SetWifiSettings() {
         //supported locators XPath, id, accessibilityID,classname,adroidUIAutomator
         driver.findElement(new AppiumBy.ByAndroidUIAutomator("text(\"Preference\")")).click();
+        driver.findElement(new AppiumBy.ByAndroidUIAutomator("text(\"3. Preference dependencies\")"))
+                .click();
+        driver.findElement(By.id("android:id/checkbox")).click();
+        driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]")).click();
+        String popUpText = driver.findElement(By.id("android:id/alertTitle")).getText();
+        Assert.assertEquals(popUpText, "WiFi settings");
+        driver.findElement(By.id("android:id/edit")).sendKeys("Test Wifi");
+        driver.findElement(By.id("android:id/button1")).click();
+    }
+    @Test
+    public void LongPressMenuValidation() {
+        driver.findElement(new AppiumBy.ByAndroidUIAutomator("text(\"Views\")")).click();
+        driver.findElement(new AppiumBy.ByAndroidUIAutomator("text(\"Expandable Lists\")")).click();
+        driver.findElement(new AppiumBy.ByAndroidUIAutomator("text(\"1. Custom Adapter\")")).click();
+
+        WebElement peopleNamesElement = driver.findElement(
+                new AppiumBy.ByAndroidUIAutomator("text(\"People Names\")"));
+        longPressAction(peopleNamesElement);
+        String longPressMenuTitle = driver.findElement(By.id("android:id/title")).getText();
+        Assert.assertEquals(longPressMenuTitle, "Sample menu");
+        Assert.assertTrue(driver.findElement(
+                new AppiumBy.ByAndroidUIAutomator("text(\"Sample menu\")")).isDisplayed());
+    }
+    @Test
+    public void ScrollValidation() {
+        driver.findElement(new AppiumBy.ByAndroidUIAutomator("text(\"Views\")")).click();
+        driver.findElement(new AppiumBy.ByAndroidUIAutomator("text(\"Expandable Lists\")")).click();
+        driver.findElement(new AppiumBy.ByAndroidUIAutomator("text(\"1. Custom Adapter\")")).click();
+
+        WebElement peopleNamesElement = driver.findElement(
+                new AppiumBy.ByAndroidUIAutomator("text(\"People Names\")"));
+        longPressAction(peopleNamesElement);
+        String longPressMenuTitle = driver.findElement(By.id("android:id/title")).getText();
+        Assert.assertEquals(longPressMenuTitle, "Sample menu");
+        Assert.assertTrue(driver.findElement(
+                new AppiumBy.ByAndroidUIAutomator("text(\"Sample menu\")")).isDisplayed());
     }
 }
--- src/main/java/sianova/BaseTest.java
+++ src/main/java/sianova/BaseTest.java
@@ -1,9 +1,12 @@
 package sianova;
 
+import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -11,6 +14,7 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.time.Duration;
 
 public class BaseTest {
 
@@ -33,6 +37,13 @@ public class BaseTest {
         options.setApp("E:\\AppiumTestProjectIntelliJ\\src\\main\\java\\resources\\ApiDemos-debug.apk");
 
         driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    public void longPressAction(WebElement element) {
+        driver.executeScript("mobile: longClickGesture",
+                ImmutableMap.of("elementId", ((RemoteWebElement) element).getId(),
+                        "duration", 2000));
     }
 
     @AfterClass
